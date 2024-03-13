8013ddfda11614dfa94d2e97838e3754785efeee
yaaaaangao <yaaaaangao@tencent.com>
2024-03-13 16:44:10 -0700

    trouble-nvim & jqx

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,6 +12,8 @@ return {
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.split-and-window.minimap-vim" },
   { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.programming-language-support.nvim-jqx" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
